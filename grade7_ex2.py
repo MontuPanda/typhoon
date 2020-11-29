4f293d6d54f9d03c28041c9b687acd67da239141
MontuPanda <75202020+MontuPanda@users.noreply.github.com>
2020-11-29 18:52:11 +0800

    first one

--- grade7_ex2.py
+++ grade7_ex2.py
@@ -1,2 +1,10 @@
 print("Hello Folks - this is Grade 7 Ex 2 for Python")
 print("---------------------------------------------")
+
+def fib(n):
+    fibs = [0, 1]
+    for i in range(2,n+1):
+        fibs.append(fibs[-1]+fibs[-2])
+    return fibs
+
+print(fib(50))  
